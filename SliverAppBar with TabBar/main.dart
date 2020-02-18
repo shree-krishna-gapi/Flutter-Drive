06d66819a1b2fe7cc8d6fa6289dd8d9a74015836
shree krishna <39576079+shree-krishna-gapi@users.noreply.github.com>
2020-02-18 18:09:15 +0545

    Add files via upload

---
+++ SliverAppBar with TabBar/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter SliverAppBar',
+      theme: ThemeData(
+        primaryColor: Color(0xff4caf50),
+        primaryColorDark: Color(0xff388E3C),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var _tabs = <String>[];
+    _tabs = <String>[
+      "Tab 1",
+      "Tab 2",
+      "Tab 3",
+    ];
+
+    return Scaffold(
+      body:
+
+      /// 加TabBar
+      DefaultTabController(
+        length: _tabs.length, // This is the number of tabs.
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            // These are the slivers that show up in the "outer" scroll view.
+            return <Widget>[
+              SliverOverlapAbsorber(
+                handle:
+                NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                child: SliverAppBar(
+                  leading: new IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {},
+                  ),
+                  title: const Text('Demo'),
+                  centerTitle: false,
+                  pinned: true,
+                  floating: false,
+                  snap: false,
+                  primary: true,
+                  expandedHeight: 230.0,
+
+                  elevation: 10,
+
+                  forceElevated: innerBoxIsScrolled,
+
+                  actions: <Widget>[
+                    new IconButton(
+                      icon: Icon(Icons.more_horiz),
+                      onPressed: () {
+                        print("Click");
+                      },
+                    ),
+                  ],
+
+                  flexibleSpace: new FlexibleSpaceBar(
+                    background: Image.network(
+                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+            fit: BoxFit.cover,
+            ),
+                  ),
+
+                  bottom: TabBar(
+                    tabs: _tabs.map((String name) => Tab(text: name)).toList(),
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(
+            // These are the contents of the tab views, below the tabs.
+            children: _tabs.map((String name) {
+              //SafeArea
+              return SafeArea(
+                top: false,
+                bottom: false,
+                child: Builder(
+                  builder: (BuildContext context) {
+                    return CustomScrollView(
+                      key: PageStorageKey<String>(name),
+                      slivers: <Widget>[
+                        SliverOverlapInjector(
+                          handle:
+                          NestedScrollView.sliverOverlapAbsorberHandleFor(
+                              context),
+                        ),
+                        SliverPadding(
+                          padding: const EdgeInsets.all(10.0),
+                          sliver: SliverFixedExtentList(
+                            itemExtent: 50.0, //item
+                            delegate: SliverChildBuilderDelegate(
+                                  (BuildContext context, int index) {
+                                return ListTile(
+                                  title: Text('Item $index'),
+                                );
+                              },
+                              childCount: 30,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+
+
+    );
+  }
+}
